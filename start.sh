90faaa3b2daf61c5ba6880ea72e914984d3ccb54
benja <benja@maletin.(none)>
2014-03-01 22:20:39 +0100

    update spigot and mcore

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #java -d64 -Xincgc -Xmx1024M -jar craftbukkit-1.7.2-R0.1-20131201.031937-2.jar nogui
-java -d64 -Xincgc -Xmx1024M -jar spigot-1.7.2-R0.1-SNAPSHOT.jar nogui
+java -d64 -Xincgc -Xmx1024M -XX:MaxPermSize=128M -jar spigot-1.7.2-R0.4-SNAPSHOT.jar nogui
