a9aeba6bd2837f4a8f5999d724f2e78f6f3a585b
benja <benja@maletin.(none)>
2014-03-30 12:27:01 +0200

    ench opples, spanwer spawns limit, silkspawners, restart server after stop

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 #java -d64 -Xincgc -Xmx1024M -jar craftbukkit-1.7.2-R0.1-20131201.031937-2.jar nogui
 java -d64 -Xincgc -Xmx3072M -XX:MaxPermSize=128M -jar spigot-1.7.2-R0.4-SNAPSHOT.jar nogui
+
+echo "Server stopped, waiting 1 second..." >> logs/latest
+
+sleep 1
+
+echo "Starting server..." >> logs/latest
+
+./start.sh
